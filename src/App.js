56279bf5e61fac4e2a5fcefa1ac6fbe54a09c5c3
Nabil Muhammad Bawahab <nabilbawahab@gmail.com>
2025-04-25 17:47:57 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const totalStudents = 10;
+  const totalAspect = 4;
+
+  const [grade, setGrade] = useState(
+    Array.from({ length: totalStudents }, () => Array(totalAspect).fill(1)),
+  );
+
+  const [jsonOutput, setJsonOutput] = useState('');
+
+  const handleChange = (studentIndex, aspectIndex, value) => {
+    const newGrade = [...grade];
+    newGrade[studentIndex][aspectIndex] = parseInt(value);
+    setGrade(newGrade);
+  };
+  const handleSubmit = (submitState) => {
+    const output = {};
+
+    Array.from({ length: totalAspect }).forEach((_, indexAspect) => {
+      const aspectKey = `aspek_penilaian_${indexAspect + 1}`;
+      output[aspectKey] = {};
+
+      grade.forEach((studentGradeList, gradeIndex) => {
+        const studentGradeKey = `mahasiswa_${gradeIndex + 1}`;
+        output[aspectKey][studentGradeKey] = studentGradeList[indexAspect];
+      });
+    });
+
+    const stringOutput = JSON.stringify(output, null, 2);
+
+    if (submitState === 'show') {
+      console.log(stringOutput);
+      setJsonOutput(stringOutput);
+
+      alert('Cek console untuk hasil JSON');
+    }
+
+    if (submitState === 'download') {
+      const blob = new Blob([stringOutput], { type: 'application/json' });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = 'penilaian-mahasiswa.json';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      URL.revokeObjectURL(url);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Aplikasi Penilaian Mahasiswa</h2>
+      <div className="table">
+        <div className="header">Mahasiswa</div>
+        {[...Array(totalAspect)].map((_, index) => (
+          <div key={index} className="header">
+            Aspek Penilaian {index + 1}
+          </div>
+        ))}
+
+        {grade.map((items, studentIndex) => (
+          <>
+            <div className="cell" key={`mhs-${studentIndex}`}>
+              Mahasiswa {studentIndex + 1}
+            </div>
+            {items.map((item, aspectIndex) => (
+              <div
+                className="cell"
+                key={`mhs-${studentIndex}-aspect-${aspectIndex}`}
+              >
+                <select
+                  value={item}
+                  onChange={(e) =>
+                    handleChange(studentIndex, aspectIndex, e.target.value)
+                  }
+                >
+                  {Array.from({ length: totalStudents }).map((_, index) => (
+                    <option key={index + 1} value={index + 1}>
+                      {index + 1}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            ))}
+          </>
+        ))}
+      </div>
+
+      <button
+        onClick={() => {
+          handleSubmit('show');
+        }}
+      >
+        Show data
+      </button>
+      <button
+        onClick={() => {
+          handleSubmit('download');
+        }}
+      >
+        Download data
+      </button>
+
+      {jsonOutput && (
+        <div>
+          <h3>Hasil JSON</h3>
+          <pre>{jsonOutput}</pre>
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
